9a1cee1cf193a1a05bc3b23e179a25cd96fb3aca
davidedelerma <davide.delerma@gmail.com>
2016-06-07 16:11:32 +0100

    working with strings

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -17,3 +17,15 @@ end
 def divide(int1,int2)
   return int1 / int2
 end
+
+def length_of_string(text)
+  return text.to_s.length
+end
+
+def join_string(text1,text2)  
+  return text1.to_s+text2.to_s
+end
+
+def add_string_as_number(text1,text2)
+  return text1.to_i+text2.to_i
+end
