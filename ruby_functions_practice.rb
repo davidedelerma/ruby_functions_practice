6b4e3fba190f24361cc0ae9eb26e55ccae72f0cc
davidedelerma <davide.delerma@gmail.com>
2016-06-07 16:21:13 +0100

    working with moths

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,64 @@ end
 def add_string_as_number(text1,text2)
   return text1.to_i+text2.to_i
 end
+
+def number_to_full_month_name(num)
+  case num
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"  
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
+end
+
+def number_to_short_month_name(num)
+    case num
+    when 1
+      return "Jan"
+    when 2
+      return "Feb"
+    when 3
+      return "Mar"
+    when 4
+      return "Apr"
+    when 5
+      return "May"
+    when 6
+      return "Jun"
+    when 7
+      return "Jul"
+    when 8
+      return "Aug"
+    when 9
+      return "Sep"  
+    when 10
+      return "Oct"
+    when 11
+      return "Nov"
+    when 12
+      return "Dec"
+    end
+  end
+
+
+
