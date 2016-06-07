5c2f94487dc825ea8d1c7718a547f4d0455a65c4
davidedelerma <davide.delerma@gmail.com>
2016-06-07 16:03:04 +0100

    math operations

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,3 +5,15 @@ end
 def add(int1,int2)
   return int1+int2
 end
+
+def subtract(int1,int2)
+  return int1-int2
+end
+
+def multiply(int1,int2)
+  return int1*int2
+end
+
+def divide(int1,int2)
+  return int1 / int2
+end
